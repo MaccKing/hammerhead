0c84a0e588dc4090d0c2ceb65967529bb82570c5
Yiduo Wang <yiduow@codeaurora.org>
2014-04-16 16:06:18 -0700

    msm: camera: Fix race condition between camera driver and camera daemon
    Delete checking command list before calling
    wait_for_competion_timeout which could cause race condtion
    if camera daemon may respond fast and there is one context swtich
    between msm_enqueue and completion.
    
    Signed-off-by: Yiduo Wang <yiduow@codeaurora.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Joao Paulo <joaolrpaulo@gmail.com>
    Signed-off-by: João Paulo <joaolrpaulo@gmail.com>
    
    Conflicts:
    	drivers/media/platform/msm/camera_v2/msm.c

--- drivers/media/platform/msm/camera_v2/msm.c
+++ drivers/media/platform/msm/camera_v2/msm.c
@@ -704,6 +704,9 @@ int msm_post_event(struct v4l2_event *event, int timeout)
 		return -EIO;
 	}
 
+	/*re-init wait_complete */
+	INIT_COMPLETION(cmd_ack->wait_complete);
+
 	v4l2_event_queue(vdev, event);
 
 	if (timeout < 0) {
@@ -714,13 +717,8 @@ int msm_post_event(struct v4l2_event *event, int timeout)
 	}
 
 	/* should wait on session based condition */
-	do {
-		rc = wait_event_interruptible_timeout(cmd_ack->wait,
-			!list_empty_careful(&cmd_ack->command_q.list),
+	rc = wait_for_completion_timeout(&cmd_ack->wait_complete,
 			msecs_to_jiffies(timeout));
-		if (rc != -ERESTARTSYS)
-			break;
-	} while (1);
 
 	if (list_empty_careful(&cmd_ack->command_q.list)) {
 		if (!rc) {
